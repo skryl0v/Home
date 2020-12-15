aad092809aed3e5f00842b19edc73edde53414e9
Sergey <skrylov67@gmail.com>
2020-12-15 11:57:11 +0300

    Add steny

--- HomeClass.java
+++ HomeClass.java
@@ -2,5 +2,6 @@ public class HomeClass{
 	public static void main(String[] argv){
 		System.out.println("Init Home!");
 		System.out.println("Заложен фундамент!");
+		System.out.println("Построили стены!");
 	}
 }
