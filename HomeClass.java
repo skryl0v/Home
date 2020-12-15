fd4c4ae9d35c2cb8fcde55499313693fdeda0167
Sergey <skrylov67@gmail.com>
2020-12-15 11:46:34 +0300

    Sad posazshen

--- HomeClass.java
+++ HomeClass.java
@@ -1,16 +1,8 @@
 public class HomeClass{
-	public class Krysha(){
-		public Krysha(){
-			buildKrysha();	
-		}
-		public void buildKrysha(){
-			System.out.println("Заложен фундамент!");
-		}
-	}
-	
 	public static void main(String[] argv){
 		System.out.println("Init Home!");
 		System.out.println("Заложен фундамент!");
-		Krysha krysha = new Krysha();
+		
+		System.out.println("Сад посажен!");
 	}
 }
