91a0df7dcf31b00ff8a9412acdcc3e472606f422
Sergey <skrylov67@gmail.com>
2020-12-15 13:06:11 +0300

    get rebase

--- HomeClass.java
+++ HomeClass.java
@@ -5,6 +5,10 @@ public class HomeClass{
 		System.out.println("Init Home!");
 		System.out.println("Заложен фундамент!");
 		System.out.println("Построили стены!");
+<<<<<<< HEAD
 		System.out.println("Построили крышу!");
+=======
+		System.out.println("Построили сад!");
+>>>>>>> f1a849e (add sad)
 	}
 }
