18412e067a8075c5709ef07169d7db6583170706
Sergey <skrylov67@gmail.com>
2020-12-15 11:39:45 +0300

    Add Krysha

--- HomeClass.java
+++ HomeClass.java
@@ -1,6 +1,16 @@
 public class HomeClass{
+	public class Krysha(){
+		public Krysha(){
+			buildKrysha();	
+		}
+		public void buildKrysha(){
+			System.out.println("Заложен фундамент!");
+		}
+	}
+	
 	public static void main(String[] argv){
 		System.out.println("Init Home!");
 		System.out.println("Заложен фундамент!");
+		Krysha krysha = new Krysha();
 	}
 }
